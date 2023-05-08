671ebce7014892e9a52d04d53e2eed9f497e5f6d
Haharmony <kevin_alcocer_22@hotmail.com>
2023-05-08 13:18:29 -0600

    Loop list Update

--- Assets/Scripts/LinkedLists/LinkedList.cs
+++ Assets/Scripts/LinkedLists/LinkedList.cs
@@ -25,8 +25,8 @@ public class LinkedList : MonoBehaviour
         firstNode.next = head;
         head.prev = firstNode;
         head = firstNode;
-
     }
+
     // Method to add a new node to the beginning of the list.
     public void AddBeginning(int data)
     {
@@ -130,13 +130,16 @@ public class LinkedList : MonoBehaviour
         }
     }
 
+    // Method to delete a node no matter the position in the linked list.
     public void DeleteNode(Node nodeToDelete)
     {
+        // If the node reference or head is null, returns.
         if (head == null || nodeToDelete == null)
         {
             return;
         }
 
+        // If the node reference is the first node, moves the head to the next node and makes the first node null.
         if (nodeToDelete == head)
         {
             head = head.next;
@@ -144,6 +147,7 @@ public class LinkedList : MonoBehaviour
             return;
         }
 
+        // If the node reference is the last node, takes the next of the prev node and makes it null.
         if (nodeToDelete.next == null)
         {
             nodeToDelete.prev.next = null;
@@ -154,6 +158,7 @@ public class LinkedList : MonoBehaviour
         nodeToDelete.next.prev = nodeToDelete.prev;
     }
 
+    // Method to find a node by the value of the node data, returns a node.
     public Node FindNode(int value)
     {
         Node nodeToFind = head;
@@ -169,6 +174,168 @@ public class LinkedList : MonoBehaviour
         return null;
     }
 
+    // Method to get the value of the node referenced by an index (position in the list), returns a node.
+    public Node GetNodeValue(int index)
+    {
+        Node nodeValue = head;
+        int currentIndex = 0;
+        while (nodeValue != null && currentIndex < index)
+        {
+            nodeValue = nodeValue.next;
+            currentIndex++;
+        }
+
+        return nodeValue;
+    }
+}
+
+public class LoopList
+{
+    public Node head;
+
+    // Initialize the list.
+    public LoopList()
+    {
+        head = null;
+    }
+
+    // Method to add a new node to the beginning of the list.
+    /*public void AddBeginning(int data)
+    {
+        Node newNode = new Node(data);
+
+        if (head == null)
+        {
+            // If the list is empty, make the new node the head.
+            head = newNode;
+        }
+        else
+        {
+            // Set the new node's next pointer to the current head.
+            newNode.next = head;
+
+            // Set the current head's prev pointer to the new node.
+            head.prev = newNode;
+
+            // Make the new node the new head of the list.
+            head = newNode;
+        }
+    }*/
+
+    // Method to add a new node to the end of the list.
+    /*public void AddLast(int data)
+    {
+        Node newNode = new Node(data);
+
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            // Run through the list to find the last node.
+            Node lastNode = head;
+            while (lastNode.next != null)
+            {
+                lastNode = lastNode.next;
+            }
+
+            // Add the new node to the end of the list.
+            lastNode.next = newNode;
+            newNode.prev = lastNode;
+        }
+    }*/
+
+    // Method to add the first node of the list.
+    public void AddFirstNode(int data)
+    {
+        Node firstNode = new Node(data);
+        if (head == null)
+        {
+            head = firstNode;
+        }
+
+        firstNode.next = head;
+        head.prev = firstNode;
+        head = firstNode;
+        head.prev = head;
+        head.next = head;
+    }
+
+    // Method to add a new node in between two existing nodes in the list.
+    public void AddInBetween(int data, int index)
+    {
+        Node newNode = new Node(data);
+
+        if (head == null)
+        {
+            AddFirstNode(data);
+            return;
+        }
+
+        // Run through the list to find the node at the specified index.
+        Node currentNode = head;
+        int currentIndex = 0;
+        while (currentIndex < index)
+        {
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+
+        newNode.prev = currentNode.prev;
+        newNode.next = currentNode;
+        currentNode.prev.next = newNode;
+        currentNode.prev = newNode;
+        head = newNode;
+    }
+
+    // Method to print the list stored data.
+    public void PrintList()
+    {
+        Node current = head;
+        while (current != null)
+        {
+            Debug.Log(current.data + " ");
+            current = current.next;
+        }
+    }
+
+    // Method to delete a node no matter the position in the linked list.
+    public void DeleteNode(Node nodeToDelete)
+    {
+        // If the node reference or head is null, returns.
+        if (head == null || nodeToDelete == null)
+        {
+            return;
+        }
+
+        if (nodeToDelete == head)
+        {
+            head = head.next;
+            head.prev = null;
+            return;
+        }
+
+        nodeToDelete.prev.next = nodeToDelete.next;
+        nodeToDelete.next.prev = nodeToDelete.prev;
+    }
+
+    // Method to find a node by the value of the node data, returns a node.
+    public Node FindNode(int value)
+    {
+        Node nodeToFind = head;
+        while (nodeToFind != null)
+        {
+            if (value == nodeToFind.data)
+            {
+                return nodeToFind;
+            }
+            nodeToFind = nodeToFind.next;
+        }
+        return null;
+    }
+
+    // Method to get the value of the node referenced by an index (position in the list), returns a node.
     public Node GetNodeValue(int index)
     {
         Node nodeValue = head;
